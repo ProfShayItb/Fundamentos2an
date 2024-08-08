2c577ad2f77e0cd9959cb11ec5afdc34ea1eb48c
Sharlayne Angel Jardini Fonseca <sharlayne.fonseca@docente.fieb.edu.br>
2024-08-08 19:40:28 -0300

    08/08

--- src/components/basicos/Nome.jsx
+++ src/components/basicos/Nome.jsx
@@ -1,13 +1,17 @@
-
+import './nome.css'
 
 export default function Nome(){
 
+    //var, let, const
+
+    let nomeDisciplina = 'DSA2'
 
     return(
        <div>
-            <h1>Sharlayne</h1>
+            <h1 id="texto">Sharlayne</h1>
             <h2>sha.jardini@gmail.com</h2>
             <h3>11111111111</h3>
+            <h3>{nomeDisciplina}</h3>
        </div>
 
     )
