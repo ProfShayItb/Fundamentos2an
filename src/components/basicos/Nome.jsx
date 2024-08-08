430e1dd395ed84e15b92c3fd459c3409b206dfa3
Sharlayne Angel Jardini Fonseca <sharlayne.fonseca@docente.fieb.edu.br>
2024-08-08 19:23:56 -0300

    08/08

---
+++ src/components/basicos/Nome.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Nome(){
+
+
+    return(
+       <div>
+            <h1>Sharlayne</h1>
+            <h2>sha.jardini@gmail.com</h2>
+            <h3>11111111111</h3>
+       </div>
+
+    )
+}
---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Primeiro(){
+
+
+
+    return(
+        <div>
+            <h1>React</h1>
+            <h2>Primeiro componente</h2>
+        </div>
+    )
+}
+
